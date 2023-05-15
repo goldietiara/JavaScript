b32cc973a2f35f320b195fc9955bf278f5de3068
goldietiara <goldietiara.acc@gmail.com>
2023-05-15 17:42:18 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,18 @@
+// variable
+var variable = "odi";
+console.log(variable);
+
+variable = variable + " bau";
+
+console.log(variable);
+
+// constant gabisa di manipulasi
+const ryan = "ryan fadhilah";
+// ryan = ryan + " bau";
+
+console.log(ryan);
+
+var number = "27";
+number = number + 27
+console.log(number)
+
