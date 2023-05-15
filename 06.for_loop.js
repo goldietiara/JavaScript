eb412722c82a542b847fb0b182f5136d9c721a65
goldietiara <goldietiara.acc@gmail.com>
2023-05-15 21:48:05 +0700

    for loop

---
+++ 06.for_loop.js
@@ -0,0 +1,26 @@
+
+
+function index_check(nama){
+
+    for(let i=0;i<=nama.length -1;i++){
+        console.log(`${nama[i]} ${i+1}`)
+    }
+}
+
+let odi = "goldie"
+console.log(odi.length)
+index_check(odi)
+console.log(odi[0])
+console.log(odi[odi.length-1])
+console.log(odi[odi.length/2])
+
+console.log(odi[0] = "R")
+
+let arr = ["g","o","l","d","i","e"]
+arr[0] += "R"
+arr[arr.length-1] = "M"
+console.log(arr)
+arr[0] += "lj"
+console.log(arr)
+arr[0] += "lj"
+console.log(arr)
